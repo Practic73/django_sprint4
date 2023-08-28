ce08411014d4f57280dad1ab6d2beaed3f650dab
Борис <sierrachart@yandex.ru>
2023-08-28 05:34:32 +0000

    Final1_2

--- blogicum/blog/querysets.py
+++ blogicum/blog/querysets.py
@@ -2,12 +2,10 @@ from django.utils import timezone
 from django.db.models import Count
 from blog.models import Post
 
-POST_QS = Post.objects.select_related(
-        'location', 'author'
-    ).annotate(comment_count=Count('comments')).order_by('-pub_date')
+POST_QS = Post.objects.select_related('location', 'author').annotate(
+                                     comment_count=Count('comments')
+                                      ).order_by('-pub_date')
 
-POST_QS_FILTER = POST_QS.filter(
-    is_published=True,
-    category__is_published=True,
-    pub_date__lte=timezone.now()
-)
+POST_QS_FILTER = POST_QS.filter(is_published=True,
+                                category__is_published=True,
+                                pub_date__lte=timezone.now())
